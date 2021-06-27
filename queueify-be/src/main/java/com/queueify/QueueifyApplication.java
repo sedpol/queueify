7a0c7e826cc85ab106579d8b35c36e0184c9dedb
Sedat Polat <sedpol@Sedats-MacBook-Pro.local>
2021-06-27 22:03:03 +0100

    creating be project

---
+++ queueify-be/src/main/java/com/queueify/QueueifyApplication.java
@@ -0,0 +1,13 @@
+package com.queueify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueifyApplication.class, args);
+	}
+
+}
---
+++ queueify-be/src/test/java/com/queueify/QueueifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queueify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
